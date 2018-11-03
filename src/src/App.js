a00bdb87a5a65e068e0f1c6f4f7ce50188e7d7ae
Marowalker <binhnguyen2798@gmail.com>
2018-11-03 19:42:26 +0700

    Add files via upload

---
+++ src/src/App.js
@@ -0,0 +1,86 @@
+﻿import React, { Component } from 'react';
+import './App.css';
+import background from './Background.png';
+import box from './Untitled-1.png';
+
+class LoginPage extends Component {
+    constructor() {
+        super();
+        this.state = {
+            username: '',
+            password: '',
+            error: '',
+        };
+
+        this.handlePassChange = this.handlePassChange.bind(this);
+        this.handleUserChange = this.handleUserChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.dismissError = this.dismissError.bind(this);
+    }
+
+    dismissError() {
+        this.setState({ error: '' });
+    }
+
+    handleSubmit(evt) {
+        evt.preventDefault();
+
+        if (!this.state.username) {
+            return this.setState({ error: 'Username is required' });
+        }
+
+        if (!this.state.password) {
+            return this.setState({ error: 'Password is required' });
+        }
+
+        return this.setState({ error: '' });
+    }
+
+    handleUserChange(evt) {
+        this.setState({
+            username: evt.target.value,
+        });
+    };
+
+    handlePassChange(evt) {
+        this.setState({
+            password: evt.target.value,
+        });
+    }
+
+    render() {
+        // NOTE: I use data-attributes for easier E2E testing
+        // but you don't need to target those (any css-selector will work)
+
+        return (
+
+            <div className="Login">
+                <img src={background} className="background" alt="background" />
+                <img src={box} className="white" alt="box" />
+                <form class="att" onSubmit={this.handleSubmit}>
+                    {
+                        this.state.error &&
+                        <h3 data-test="error" onClick={this.dismissError}>
+                            <button onClick={this.dismissError}>✖</button>
+                            {this.state.error}
+                        </h3>
+                    }
+                    <label id="title">Login</label>
+                    <br />
+                    <label>Username</label>
+                    <input type="text" placeholder="enter username" data-test="username" value={this.state.username} onChange={this.handleUserChange} />
+                    <br />
+                    <label>Password</label>
+                    <input type="password" placeholder="enter password" data-test="password" value={this.state.password} onChange={this.handlePassChange} />
+                    <br />
+                    <input type="checkbox" id="box" />
+                    <label class="checkbox">Remember me</label>
+                    <br />
+                    <input id="login" type="submit" value="Log In" data-test="submit" />
+                </form>
+            </div>
+        );
+    }
+}
+
+export default LoginPage;
---
+++ src/src/StudentHome.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import background from './Background.png';
+import './StudentHome.css';
+
+class StudentHomePage extends Component {
+    render() {
+        return (
+            <div>
+                <div class="left">
+                    <ul class="lmenu">
+                        <li class="items">Dashboard</li>
+                        <li class="items">User Profile</li>
+                        <li class="items">Log Out</li>
+                    </ul>
+                </div>
+                <br class="clear" />
+                <div class="center">
+                    <ul>
+                    </ul>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default StudentHomePage;
